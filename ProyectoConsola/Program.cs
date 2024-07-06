ef05314c53971129a305ad3de57e253a6f7b69b9
Gael Lara <laragael2003@gmail.com>
2024-07-06 14:21:15 -0700

    ejercicio con arreglos

--- ProyectoConsola/Program.cs
+++ ProyectoConsola/Program.cs
@@ -5,6 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("HOLA C# Y GITHUB");
+
+            string nombre = "Gael";
+            Console.WriteLine(nombre);
+
+            string[] nombres = new string[4];
+            nombres[0] = "Gael";
+            nombres[1] = "Cristo";
+            nombres[2] = "Fransisco";
+            nombres[3] = "Kevin";
+            Console.WriteLine(nombres[3]);
+
+            Console.WriteLine();
+            Console.WriteLine("Mostrando todos los elementos del arreglo");
+            Console.WriteLine();
+
+            foreach (string nombreAlumno in nombres)
+            {
+                Console.WriteLine(nombreAlumno);
+            }
+
+
+
         }
     }
 }
